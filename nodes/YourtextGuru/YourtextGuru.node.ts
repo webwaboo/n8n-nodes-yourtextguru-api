dbc5bcd75a70969445147dfdfcf5d87b616f2176
webwaboo <web.waboo@gmx.com>
2025-08-12 22:33:08 +0200

    all guides endpoint done and tested

--- nodes/YourtextGuru/YourtextGuru.node.ts
+++ nodes/YourtextGuru/YourtextGuru.node.ts
@@ -197,6 +197,9 @@ export class YourtextGuru implements INodeType {
 						request: {
 							method: 'POST',
 							url: '=/guides/{{$parameter.guideId}}/brief/analyze',
+							body: {
+								text: '{{$parameter.text}}'
+							},
 						},
 					},
 				},
@@ -211,12 +214,15 @@ export class YourtextGuru implements INodeType {
 						request: {
 							method: 'POST',
 							url: '=/guides/{{$parameter.guideId}}/check',
+							body: {
+								text: '={{$parameter.text}}'
+							}
 						},
 					},
 				},
 				// Operation : createGuide
 				{
-					name: 'Create',
+					name: 'Create Guide',
 					value: 'createGuide',
 					action: 'Create guide',
 					description: 'Create a new guide',
@@ -225,6 +231,27 @@ export class YourtextGuru implements INodeType {
 						request: {
 							method: 'POST',
 							url: '/guides',
+							qs: {
+								query: '={{$parameter.query}}',
+								lang: '={{$parameter.lang}}',
+								projectId: '={{$parameter.projectIdOptional}}',
+								type: '={{$parameter.guideType}}',
+								groupId: '={{$parameter.groupIdOptional}}',
+							}
+						},
+					},
+				},
+				// Operation : createBrief
+				{
+					name: 'Create Brief',
+					value: 'createBrief',
+					action: 'Create guide brief',
+					description: 'Create a new brief guide',
+					routing: {
+						// set method and url for the endpoint
+						request: {
+							method: 'POST',
+							url: '=/guides/{{$parameter.guideId}}/brief',
 						},
 					},
 				},
@@ -252,7 +279,7 @@ export class YourtextGuru implements INodeType {
 						// set method and url for the endpoint
 						request: {
 							method: 'GET',
-							url: '=/guides/{{$parameter.guideId}}',
+							url: '=/guides/{{$parameter.guideId}}/{{$parameter.guideTypeSearch}}',
 						},
 					},
 				},
@@ -267,6 +294,14 @@ export class YourtextGuru implements INodeType {
 						request: {
 							method: 'GET',
 							url: '/guides',
+							qs: {
+								apiOnly: '={{$parameter.apiOnly}}',
+								groupId: '={{$parameter.groupIdOptional}}',
+								lang: '={{$parameter.langOptionnal}}',
+								lastId: '={{$parameter.lastId}}',
+								projectId: '={{$parameter.projectIdOptional}}',
+								status: '={{$parameter.status}}',
+							}
 						},
 					},
 				},
@@ -500,14 +535,14 @@ export class YourtextGuru implements INodeType {
 				show: {
 					//only show if you've selected :
 					resource: ['guide'],
-					operation: ['createGuide','listGuides'],
+					operation: ['createGuide','listGuides','createGuide'],
 				},
 			},
 		},
-		// parameter : guideType
+		// parameter : guideTypeSearch
 		{
 			displayName: 'Type of Guide',
-			name: 'guideType',
+			name: 'guideTypeSearch',
 			type: 'options',
 			options :[
 				{name: 'Guide', value: ''},
@@ -516,11 +551,11 @@ export class YourtextGuru implements INodeType {
 				{name: 'Guide Related Informations', value: 'related'},
 				{name: 'Guide Serp', value: 'serp'}
 			],
-			routing: {
+			/*routing: {
 				request: {
 					url: '=/guides/{{$parameter.guideId}}/{{$value}}'
 				}
-			},
+			},*/
 			default:'',
 			displayOptions: {
 				show: {
@@ -542,7 +577,7 @@ export class YourtextGuru implements INodeType {
 				show: {
 					//only show if you've selected :
 					resource: ['guide'],
-					operation: ['getGuide','getGuideBriefAnalysis']
+					operation: ['getGuide','getGuideBriefAnalysis','check','analyzeText','createBrief','deleteGuide']
 				},
 			},
 		},
@@ -582,7 +617,215 @@ export class YourtextGuru implements INodeType {
 				}
 			}
 		},
+		// parameter : apiOnly
+		{
+			displayName: 'API Only',
+			description: 'Get only guide created by API (default: get all)',
+			name: 'apiOnly',
+			type: 'options',
+			options: [
+				{ name: 'Yes', value: 1 },
+				{ name: 'No', value: 0 }
+			],
+			default: 0,
+			displayOptions: {
+				show: {
+					//only show if you've selected :
+					resource: ['guide'],
+					operation: ['listGuides']
+				},
+			},
+		},
+		// parameter : lang
+		{
+			displayName: 'Language',
+			description: 'Language (default: get all)',
+			default: 'en_US',
+			required: true,
+			name: 'lang',
+			type: 'options',
+			options: [
+        { name: "Dutch (Netherlands)", value: "nl_NL" },
+        { name: "English (Australia)", value: "en_AU" },
+        { name: "English (Canada)", value: "en_CA" },
+        { name: "English (Egypt)", value: "en_EG" },
+        { name: "English (India)", value: "en_IN" },
+        { name: "English (South Africa)", value: "en_ZA" },
+        { name: "English (United Arab Emirates)", value: "en_AE" },
+        { name: "English (United Kingdom)", value: "en_GB" },
+        { name: "English (United States)", value: "en_US" },
+        { name: "French (Andorra)", value: "fr_AD" },
+        { name: "French (Belgium)", value: "fr_BE" },
+        { name: "French (Canada)", value: "fr_CA" },
+        { name: "French (France)", value: "fr_FR" },
+        { name: "French (Luxembourg)", value: "fr_LU" },
+        { name: "French (Morocco)", value: "fr_MA" },
+        { name: "French (Switzerland)", value: "fr_CH" },
+				{ name: "German (Austria)", value: "de_AT" },
+        { name: "German (Belgium)", value: "de_BE" },
+        { name: "German (Germany)", value: "de_DE" },
+        { name: "German (Switzerland)", value: "de_CH" },
+        { name: "Italian (Italy)", value: "it_IT" },
+        { name: "Italian (Switzerland)", value: "it_CH" },
+        { name: "Polish (Poland)", value: "pl_PL" },
+        { name: "Portuguese (Brazil)", value: "pt_BR" },
+        { name: "Portuguese (Portugal)", value: "pt_PT" },
+        { name: "Romanian (Romania)", value: "ro_RO" },
+				{ name: "Spanish (Argentina)", value: "es_AR" },
+        { name: "Spanish (Chile)", value: "es_CL" },
+        { name: "Spanish (Colombia)", value: "es_CO" },
+        { name: "Spanish (Mexico)", value: "es_MX" },
+        { name: "Spanish (Peru)", value: "es_PE" },
+        { name: "Spanish (Spain)", value: "es_ES" },
+        { name: "Spanish (United States)", value: "es_US" },
+      ],
+			displayOptions: {
+				show: {
+					//only show if you've selected :
+					resource: ['guide'],
+					operation: ['createGuide']
+				},
+			},
+		},
+		// parameter : langOptionnal
+		{
+			displayName: 'Language',
+			description: 'Language (default: get all)',
+			default: '',
+			name: 'langOptionnal',
+			type: 'options',
+			options: [
+				{ name: "All", value: ""},
+        { name: "Dutch (Netherlands)", value: "nl_NL" },
+        { name: "English (Australia)", value: "en_AU" },
+        { name: "English (Canada)", value: "en_CA" },
+        { name: "English (Egypt)", value: "en_EG" },
+        { name: "English (India)", value: "en_IN" },
+        { name: "English (South Africa)", value: "en_ZA" },
+        { name: "English (United Arab Emirates)", value: "en_AE" },
+        { name: "English (United Kingdom)", value: "en_GB" },
+        { name: "English (United States)", value: "en_US" },
+        { name: "French (Andorra)", value: "fr_AD" },
+        { name: "French (Belgium)", value: "fr_BE" },
+        { name: "French (Canada)", value: "fr_CA" },
+        { name: "French (France)", value: "fr_FR" },
+        { name: "French (Luxembourg)", value: "fr_LU" },
+        { name: "French (Morocco)", value: "fr_MA" },
+        { name: "French (Switzerland)", value: "fr_CH" },
+				{ name: "German (Austria)", value: "de_AT" },
+        { name: "German (Belgium)", value: "de_BE" },
+        { name: "German (Germany)", value: "de_DE" },
+        { name: "German (Switzerland)", value: "de_CH" },
+        { name: "Italian (Italy)", value: "it_IT" },
+        { name: "Italian (Switzerland)", value: "it_CH" },
+        { name: "Polish (Poland)", value: "pl_PL" },
+        { name: "Portuguese (Brazil)", value: "pt_BR" },
+        { name: "Portuguese (Portugal)", value: "pt_PT" },
+        { name: "Romanian (Romania)", value: "ro_RO" },
+				{ name: "Spanish (Argentina)", value: "es_AR" },
+        { name: "Spanish (Chile)", value: "es_CL" },
+        { name: "Spanish (Colombia)", value: "es_CO" },
+        { name: "Spanish (Mexico)", value: "es_MX" },
+        { name: "Spanish (Peru)", value: "es_PE" },
+        { name: "Spanish (Spain)", value: "es_ES" },
+        { name: "Spanish (United States)", value: "es_US" },
+      ],
+			displayOptions: {
+				show: {
+					//only show if you've selected :
+					resource: ['guide'],
+					operation: ['listGuides']
+				},
+			},
+		},
+		// parameter : lastId
+		{
+			displayName: 'Last ID',
+			description: 'Last guide ID to get next data (default 0)',
+			name: 'lastId',
+			type: 'number',
+			default: 0,
+			displayOptions: {
+				show: {
+					//only show if you've selected :
+					resource: ['guide'],
+					operation: ['listGuides']
+				},
+			},
+		},
+		// parameter : status
+		{
+			displayName: 'Status',
+			description: 'Get guide with a specific status',
+			name: 'status',
+			type: 'options',
+			options: [
+				{ name: "All", value: "" },
+        { name: "Error", value: "error" },
+        { name: "In Progress", value: "in_progress" },
+        { name: "Ready", value: "ready" },
+        { name: "Waiting", value: "waiting" },
+      ],
+			default: "",
+			displayOptions: {
+				show: {
+					//only show if you've selected :
+					resource: ['guide'],
+					operation: ['listGuides']
+				},
+			},
+		},
+		// parameter : text
+		{
+			displayName: 'Text',
+			description: 'A long text in UTF-8 encoding',
+			name: 'text',
+			type: 'string',
+			default: '',
+			displayOptions: {
+				show: {
+					//only show if you've selected :
+					resource: ['guide'],
+					operation: ['check','analyzeText']
+				},
+			},
+		},
+		// parameter : guideType
+		{
+			displayName: 'Type of Guide',
+			name: 'guideType',
+			type: 'options',
+			description: "Guide type (google by default)",
+			options :[
+				{name: 'Google', value: 'google'},
+				{name: 'Bing', value: 'bing'},
 
+			],
+			default:'google',
+			displayOptions: {
+				show: {
+					//only show if you've selected :
+					resource: ['guide'],
+					operation: ['createGuide'],
+				},
+			},
+		},
+		// parameter : query
+		{
+			displayName: 'Query',
+			description: 'Request used to generate a guide',
+			name: 'query',
+			required: true,
+			type: 'string',
+			default: '',
+			displayOptions: {
+				show: {
+					//only show if you've selected :
+					resource: ['guide'],
+					operation: ['createGuide']
+				},
+			},
+		},
 
 
 		// Optional/additional fields will go here, always in type collection
